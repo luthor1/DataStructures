141b1c34086770fb1b29c8d32445e5f7dbb690d7
Ali Çuğun <alicugun123@gmail.com>
2025-10-02 20:57:37 +0300

    Created doubly linked list and node.

---
+++ DataStructures/classes/DoublyLinkedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.classes
+{
+    internal class DoublyLinkedList <T>
+    {
+        private DoublyNode<T>? head;
+        private DoublyNode<T>? tail;
+        private int size;
+
+        public DoublyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+            this.size = 0;
+        }
+
+        public DoublyLinkedList(T data)
+        {
+            DoublyNode<T> newNode = new DoublyNode<T>(data);
+            this.head = newNode;
+            this.tail = newNode;
+            this.size = 1;
+        }
+    }
+}
---
+++ DataStructures/classes/DoublyNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.classes 
+{
+    internal class DoublyNode <T>
+    {
+        public T? data { get; set; }
+        public DoublyNode<T>? next { get; set; }
+        public DoublyNode<T>? prev { get; set; }
+        public DoublyNode (T data)
+        {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+}
